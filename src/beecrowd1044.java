459b923895cdec2800094a99e28233d098ff895f
paula-castro-oliveira <paulaflea@gmail.com>
2022-08-12 15:46:08 -0300

    Novos exercícios adicionados

---
+++ src/beecrowd1044.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class beecrowd1044 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if (A % B == 0 || B % A == 0) {
+			System.out.println("Sao multiplos");
+		}
+		
+		else {
+			System.out.println("Nao sao multiplos");
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
